d7695d604bd56cfb28c363cc6f7387c3833556a7
Tanki <remi007tank@gmail.com>
2025-10-19 12:47:17 +0200

    fix : taaaask

--- task.js
+++ task.js
@@ -1,10 +1,8 @@
 function toggleTask(id){
-    let newtache={"name":"julien","id":1, "done":false}
-    tasks.push(newtache);
-    const foundTache = tasks.find(newtache=>newtache.id===id);
-    if(foundTache){
-        foundTache.done=!foundTache.done;
-        return foundTache.done
+    const foundTache = tasks.find(t => t.id === id);
+    if (foundTache) {
+        foundTache.done = !foundTache.done;
+        return foundTache.done;
     }
     return "tache non trouvée";
 }
